b9d678b1c3a091cf34d3182304bb0d82b0c08676
tean <laitean19@gmail.com>
2024-12-03 18:33:40 -0500

    implement echo

--- app/main.py
+++ app/main.py
@@ -7,6 +7,11 @@ def main():
         line = input().split()
         if line[0] == "exit":
             exit(int(line[1]))
+        elif line[0] == "echo":
+            if len(line) == 0:
+                print()
+            else:
+                print(" ".join(line[1:]))
         else:
             print(f"{line[0]}: not found")
 
