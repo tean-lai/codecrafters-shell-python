60283f1fa4222c04a7ba6ae134932542f93f3212
tean <laitean19@gmail.com>
2024-12-03 18:23:58 -0500

    first stage

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ import sys
 
 
 def main():
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
+    sys.stdout.write("$ ")
 
-    # Wait for user input
     input()
 
 
